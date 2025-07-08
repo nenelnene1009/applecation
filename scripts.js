6ec842441d1c5aea7e742f9b5ede5ca488081f5e
nenelnene1009 <valucan500@gmail.com>
2025-07-08 12:22:26 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,32 +10,32 @@ const animalData = [
   {
     type: "ねこタイプ",
     description: "しなやかで気まぐれ、でも甘えん坊なあなたは「ねこ」タイプ！",
-    image: "images/cat.png"
+    image: "https://cdn.pixabay.com/photo/2021/03/23/08/36/cat-6116822_1280.png"
   },
   {
     type: "いぬタイプ",
     description: "忠実で元気いっぱい！誰とでも仲良くできる「いぬ」タイプ！",
-    image: "images/dog.png"
+    image: "https://cdn.pixabay.com/photo/2020/08/10/17/58/dog-5478480_1280.png"
   },
   {
     type: "うさぎタイプ",
     description: "おっとりマイペースで癒し系、そんなあなたは「うさぎ」タイプ！",
-    image: "images/rabbit.png"
+    image: "https://cdn.pixabay.com/photo/2021/03/09/05/46/rabbit-6078652_1280.png"
   },
   {
     type: "パンダタイプ",
     description: "のんびり屋で、周囲を和ませるあなたは「パンダ」タイプ！",
-    image: "images/panda.png"
+    image: "https://cdn.pixabay.com/photo/2022/06/14/07/38/panda-7261457_1280.png"
   },
   {
     type: "キツネタイプ",
     description: "賢くてミステリアスな雰囲気を持つあなたは「キツネ」タイプ！",
-    image: "images/fox.png"
+    image: "https://cdn.pixabay.com/photo/2021/07/04/13/26/fox-6386107_1280.png"
   },
   {
     type: "カワウソタイプ",
     description: "社交的で遊び好きなあなたは「カワウソ」タイプ！",
-    image: "images/otter.png"
+    image: "https://cdn.pixabay.com/photo/2023/04/24/12/02/otter-7948354_1280.png"
   }
 ];
 
